f1a06eef8b0231b3cf5f7d26cb2721ea4274c96c
xsl <x976403038@163.com>
2020-03-14 17:40:16 +0800

    头部组件的封装

---
+++ src/components/layout-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="my-header">
+        <span>电影列表</span>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入豆瓣的静态资源
+import '@/styles/css/global.css'
+import '@/styles/fonts/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
